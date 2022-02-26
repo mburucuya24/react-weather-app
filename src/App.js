a5498c4f3040ce8ed4edb09796705d11bb52d47f
mburucuya24 <92304084+mburucuya24@users.noreply.github.com>
2022-02-26 18:01:28 -0500

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <p>
+        <a
+          href="https://github.com/mburucuya24/weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-sourced code
+        </a>{" "}
+        by Pauline Hwang
+      </p>
+    </div>
+  );
 }
 
 export default App;
